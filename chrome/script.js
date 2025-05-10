d8fee53a27540441799675e146d989a096673abf
Super Zombi <super.zombi.yt@gmail.com>
2025-05-10 18:01:38 +0300

    2.0.0
    Bug fixes

--- chrome/script.js
+++ chrome/script.js
@@ -1,5 +1,5 @@
 var Settings = {}
-chrome.storage.sync.get({ AutoJoin: false, AutoMute: false, AutoSkipAlerts: false,
+chrome.storage.sync.get({ AutoJoin: false, AutoMute: false,
 						  Fullscreen: true, fullscreen_hotkey: true, volumeController: true,
 						  PictureInPicture: true,
 						  account: null, language: null
@@ -9,7 +9,7 @@ chrome.storage.sync.get({ AutoJoin: false, AutoMute: false, AutoSkipAlerts: fals
 						  });
 
 chrome.storage.sync.onChanged.addListener(function (changes, namespace) {
-	chrome.storage.sync.get({ AutoJoin: false, AutoMute: false, AutoSkipAlerts: false,
+	chrome.storage.sync.get({ AutoJoin: false, AutoMute: false,
 						  Fullscreen: true, fullscreen_hotkey: true, volumeController: true, PictureInPicture: true
 						  }, results => { Settings = results; main(); });
 });
@@ -38,15 +38,9 @@ function main(){
 	if (window.location.pathname != "/"){
 		if (!first_load){in_meet_main()}
 		else{
-			if (Settings.AutoSkipAlerts){
-				var interv = init_event_thrower(get_auto_skip, _=>{
-					get_auto_skip().querySelector("button").click()
-				})
-			}
-
 			if (Settings.AutoMute){
 				init_event_thrower(get_mic_and_vid_controls, _=>{
-					document.querySelectorAll('div[jsname=BOHaEe]').forEach(function(e){
+					document.querySelectorAll('div[jsname=hw0c9], div[jsname=psRWwc]').forEach(function(e){
 						if (e.getAttribute("data-is-muted") != "true"){
 							e.click();
 						}
@@ -68,10 +62,6 @@ function main(){
 			}
 
 			init_event_thrower(is_in_meeting, _=>{
-				if (Settings.AutoSkipAlerts){
-					try{clearInterval(interv)}catch{}
-					if (get_auto_skip()){get_auto_skip().querySelector("button").click()}
-				}
 				document.querySelectorAll('audio, video').forEach(function (el) {
 					el.play();
 				});
@@ -94,31 +84,28 @@ function init_event_thrower(func, callback, timeout=1000){
 }
 
 function is_in_meeting(){
-	if (document.querySelector("button[jsname=BOHaEe]")){
+	if (document.querySelector("button[jsname=CQylAd]")){
 		return true;
 	}
 	return false;
 }
 function get_join_button(){
-	return document.querySelector("button[jsname=Qx7uuf]")
-}
-function get_auto_skip(){
-	return document.querySelector('div[aria-modal="true"][role="alertdialog"]')
+	return document.querySelector("div[jsname=Qx7uuf] button")
 }
 function get_mic_and_vid_controls(){
-	let temp = document.querySelector('div[jsname=BOHaEe]')
-	if (temp){return temp.getAttribute("data-is-muted")}
-	return false
+	let mic = document.querySelector('div[jsname=hw0c9]')
+	let cam = document.querySelector('div[jsname=psRWwc]')
+	return mic && cam
 }
 
 var pictureInPictureInterval;
 function in_meet_main(){
 	var menu_items;
-	let temp = document.querySelector('div.tMdQNe')
+	let temp = document.querySelector('nav.tMdQNe')
 	if (!temp.querySelector("#meetHelperTools")){
 		menu_items = document.createElement("div")
 		menu_items.id = "meetHelperTools"
-		temp.appendChild(menu_items)
+		temp.prepend(menu_items);
 	} else{ menu_items = temp.querySelector("#meetHelperTools") }
 
 	if (Settings.volumeController){
@@ -155,13 +142,16 @@ function in_meet_main(){
 						div.querySelector("span").style.top = "-20px"
 						div.querySelector("span").style.bottom = ""
 						div.querySelector("span").style.opacity = 1;
+						div.querySelector("input").style.opacity = 1;
 					}
 					else{
 						div.querySelector("span").style.opacity = 0;
+						div.querySelector("input").style.opacity = 0;
 					}
 				} else {
 					div.querySelector("div").style.visibility = "hidden";
 					div.querySelector("span").style.opacity = 0;
+					div.querySelector("input").style.opacity = 0;
 				}
 			})
 
@@ -176,14 +166,16 @@ function in_meet_main(){
 
 			let input = document.createElement("input")
 			input.type = "range"
-			input.setAttribute("orient", "vertical");
-			input.style.webkitAppearance = "slider-vertical";
+			input.style.writingMode = "vertical-lr"
+			input.style.direction = "rtl"
 			input.min = 0
 			input.max = 1
 			input.step = 0.1
 			input.value = 1
 			input.style.width = "6px"
 			input.style.cursor = "ns-resize"
+			input.style.transition = "0.4s"
+			input.style.opacity = 0;
 
 			function on_input_change(input_){
 				currentVolume = input_.value
@@ -225,7 +217,7 @@ function in_meet_main(){
 			text.style.top = "-20px";
 			text.style.left = "50%";
 			text.style.transform = "translateX(-50%)";
-			text.style.transition = "0.5s"
+			text.style.transition = "0.4s"
 			text.style.pointerEvents = "none";
 			text.style.visibility = "visible"
 			text.style.opacity = 0;
